4a76c006979cc2ad7f0d6753958fc125d04c17df
Jordan Irwin <antumdeluge@gmail.com>
2019-02-07 06:29:13 +0000

    Fix shutdown function

--- scripts/main.lua
+++ scripts/main.lua
@@ -14,16 +14,26 @@ end
 UNSTABLE = true
 
 -- run as standalone executable
-local standalone = TrainerOrigin ~= nil
+local standalone = TrainerOrigin == nil
 
 -- create main window but do not show it yet
 local MainWindow = createForm(false)
 
+-- closes trainer
+local function shutdown()
+	-- free memory allocated for the main interface
+	MainWindow.destroy()
+
+	-- trainer is run as a standalone executable
+	if standalone then
+		-- shuts down the main CE process
+		closeCE()
+		return caFree
+	end
+end
+
 -- text displayed in title bar
 MainWindow.setCaption("MMU Trainer")
-
--- action to take when "X-ed" out of
-MainWindow.onClose = shutdown
 
 
 --- START: Menu Bar ---
@@ -67,18 +77,8 @@ MainWindow.setMenu(menuBar)
 --- END: Menu Bar ---
 
 
--- closes trainer
-local function shutdown()
-	-- free memory allocated for the main interface
-	MainWindow.destroy()
-
-	-- trainer is run as a standalone executable
-	if TrainerOrigin == nil then
-		-- shuts down the main CE process
-		closeCE()
-		return caFree
-	end
-end
+-- action to take when "X-ed" out of
+MainWindow.onClose = shutdown
 
 -- make main window visible
 MainWindow.centerScreen()
