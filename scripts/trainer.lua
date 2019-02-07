86aedf5c1161f789cb9fa65661841445c10af779
Jordan Irwin <antumdeluge@gmail.com>
2019-02-07 03:06:46 +0000

    Create menu bar for standalone trainer

--- scripts/trainer.lua
+++ scripts/trainer.lua
@@ -4,7 +4,6 @@ local standalone = TrainerOrigin ~= nil
 
 -- create main window but do not show it yet
 local MainWindow = createForm(false)
-MainWindow.setCaption("MMU Trainer")
 
 -- closes trainer
 local function shutdown()
@@ -18,8 +17,38 @@ local function shutdown()
 	end
 end
 
+-- text displayed in title bar
+MainWindow.setCaption("MMU Trainer")
+
 -- action to take when "X-ed" out of
 MainWindow.onClose = shutdown
+
+
+--- START: Menu Bar ---
+
+-- create a menu bar for main window
+local menuBar = createMainMenu(MainWindow)
+local menuBarItems = menuBar.getItems()
+
+-- "File" menu
+local menuFile = createMenuItem(menuBar)
+menuFile.setCaption("File")
+
+-- "Open" menu item
+local menuFileOpen = createMenuItem(menuFile)
+menuFileOpen.setCaption("Open")
+menuFileOpen.onClick = function()
+end
+
+menuFile.add(menuFileOpen)
+
+-- add items to menu bar
+menuBar.Items.add(menuFile)
+
+MainWindow.setMenu(menuBar)
+
+--- END: Menu Bar ---
+
 
 -- make main window visible
 MainWindow.show()
