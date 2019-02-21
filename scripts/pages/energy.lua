4bdae8490d1e53bb757fafca80458e0399145f2e
Jordan Irwin <antumdeluge@gmail.com>
2019-02-21 11:38:43 -0800

    Trainer: Add Infinite Weapon Energy check box to Energy page

--- scripts/pages/energy.lua
+++ scripts/pages/energy.lua
@@ -1,4 +1,22 @@
+
 local pgEnergy = tabs.addTab()
 pgEnergy.setCaption('Energy')
+
+--- Record for infinite weapon energy
+local recInfinite = record.get('Infinite Weapon Energy')
+
+-- Setup GUI
+
+local chkInfinite = createCheckBox(pgEnergy)
+chkInfinite.setCaption(recInfinite.Description)
+if recInfinite.Active then
+	chkInfinite.setState(cbChecked)
+else
+	chkInfinite.setState(cbUnchecked)
+end
+
+chkInfinite.OnChange = function()
+	recInfinite.Active = chkInfinite.Checked
+end
 
 return pgEnergy
