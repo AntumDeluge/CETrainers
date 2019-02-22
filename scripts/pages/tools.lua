febe24e5548c45fdf520c877741857508ba9756f
Jordan Irwin <antumdeluge@gmail.com>
2019-02-21 16:12:46 -0800

    Control tools/weapon state with Trainer

--- scripts/pages/tools.lua
+++ scripts/pages/tools.lua
@@ -18,6 +18,42 @@ for idx, rec in ipairs(recTools.Child) do
 		chk.Checked = rec.Value == '1'
 	end
 
+	-- polls record value to catch changes
+	local prevValue = nil
+	rec.OnGetDisplayValue = function(sender, curValue)
+		local changed = prevValue and (prevValue ~= curValue)
+		prevValue = curValue
+
+		if changed then
+			chk.OnChange(rec)
+		end
+
+		return false, curValue
+	end
+
+	-- function to prevent redundant changes to value & check box state
+	local synchronized = function()
+		if (not chk.Checked and rec.Value == '0') or (chk.Checked and rec.Value ~= '0') then
+			return true
+		end
+
+		return false
+	end
+
+	chk.OnChange = function(sender)
+		if not synchronized() then
+			if sender == chk then
+				if chk.Checked then
+					rec.Value = '1'
+				else
+					rec.Value = '0'
+				end
+			elseif sender == rec then
+				chk.Checked = rec.Value ~= '0'
+			end
+		end
+	end
+
 	if prevChk ~= nil then
 		chk.anchorSideTop.control = prevChk
 		chk.anchorSideTop.side = asrBottom
