bec9dd456288d0a2cc1b6bcea4100e839ce71001
Jordan Irwin <antumdeluge@gmail.com>
2019-02-25 11:47:16 -0800

    Trainer: Put menu bar in separate module

---
+++ scripts/menu.lua
@@ -0,0 +1,56 @@
+
+-- create a menu bar for main window
+local menuBar = createMainMenu(MainWindow)
+local menuBarItems = menuBar.getItems()
+
+-- 'File' main menu
+local menuFile = createMenuItem(menuBar)
+menuFile.setCaption('File')
+
+-- 'Open' menu item
+local miOpen = createMenuItem(menuFile)
+miOpen.setCaption('Open Process')
+miOpen.ShortCut = 16463
+miOpen.onClick = function()
+	local process = dofile('scripts/process.lua')
+	local PID = process.attach()
+	if PID ~= nil then
+		mmu.processLabel.setCaption('Attached process: ' .. tostring(PID))
+	end
+end
+
+local bmpOpen = createPicture()
+bmpOpen.loadFromFile('data/bitmap/menu/process.png')
+miOpen.Bitmap = bmpOpen.getBitmap()
+
+-- 'Quit' menu item
+local miQuit = createMenuItem(menuFile)
+miQuit.setCaption('Quit')
+miQuit.onClick = shutdown
+
+local bmpQuit = createPicture()
+bmpQuit.loadFromFile('data/bitmap/menu/quit.png')
+miQuit.Bitmap = bmpQuit.getBitmap()
+
+menuFile.add(miOpen)
+menuFile.add(miQuit)
+
+-- 'Help' main menu
+local menuHelp = createMenuItem(menuBar)
+menuHelp.setCaption('Help')
+
+-- 'About' menu item
+local miAbout = createMenuItem(menuHelp)
+miAbout.setCaption('About')
+miAbout.onClick = function()
+	local about = dofile('scripts/about.lua')
+	about.showDialog()
+end
+
+menuHelp.add(miAbout)
+
+-- add items to menu bar
+menuBar.Items.add(menuFile)
+menuBar.Items.add(menuHelp)
+
+MainWindow.setMenu(menuBar)
