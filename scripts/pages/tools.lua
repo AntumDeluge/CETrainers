9e2366500b2e3f75998c1c140c87b816d30f4869
Jordan Irwin <antumdeluge@gmail.com>
2019-02-25 23:26:05 -0800

    Trainer: Fix tools page to include buster weapon

--- scripts/pages/tools.lua
+++ scripts/pages/tools.lua
@@ -7,9 +7,10 @@ tools.AutoScroll = true
 local parentID = 215
 local recTools = record.get(parentID, true)
 
-for idx, rec in ipairs(recTools.Child) do
-	-- FIXME: not all controls working
-	createControl('checkvalue', rec.Description, tools)
+local idx = 0
+while idx < recTools.Count do
+	createControl('checkvalue', recTools[idx], tools)
+	idx = idx + 1
 end
 
 return tools
