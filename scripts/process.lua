3468cf82f8e97c6b0185b88be16864ca5ce1bf0a
Jordan Irwin <antumdeluge@gmail.com>
2019-02-25 20:51:19 -0800

    Trainer: Show progress dialog when opening process

--- scripts/process.lua
+++ scripts/process.lua
@@ -117,25 +117,150 @@ process.setName = function(pName)
 	return pName, modalResult
 end
 
--- attaches process
-process.attach = function(pName)
-	local wList = getWindowList()
+--- Creates a dialog to show progress.
+local createProgressDialog = function(pName)
+	local dialog = createForm(false)
+	dialog.setCaption('Open Process')
+	dialog.BorderStyle = bsDialog
 
-	local proc = nil
+	dialog.Width = 200
+	dialog.Height = 100
 
-	for _, w in pairs(wList) do
-		if w.Name == pName then
-			proc = w
-			break
+	--[[
+	dialog.OnClose = function()
+		-- '6' is the value returned when mbYes is pressed
+		if messageDialog('Are you sure you want to cancel?', mtInformation, mbYes, mbNo) == 6 then
+			return dialog.ModalResult
+		end
+		return dialog.ModalResult
+	end
+	]]
+
+	local label = createLabel(dialog)
+	label.setCaption('Searching for process: ' .. pName)
+	label.AnchorSideLeft.Control = dialog
+	label.AnchorSideLeft.Side = asrCenter
+
+	local progress = createProgressBar(dialog)
+	progress.Anchors = '[akLeft,akRight,akBottom]'
+	progress.AnchorSideBottom.Control = dialog
+	progress.AnchorSideBottom.Side = asrCenter
+	progress.AnchorSideRight.Control = dialog
+	progress.AnchorSideRight.Side = asrRight
+	progress.setMax(5)
+
+	--[[
+	local btnCancel = createButton(dialog)
+	btnCancel.setCaption('Cancel')
+	btnCancel.Anchors = '[akLeft,akBottom]'
+	btnCancel.AnchorSideBottom.Control = dialog
+	btnCancel.AnchorSideBottom.Side = asrBottom
+	btnCancel.AnchorSideLeft.Control = dialog
+	btnCancel.AnchorSideLeft.Side = asrCenter
+
+	btnCancel.OnClick = dialog.close
+	]]
+
+	centerOnMainWindow(dialog)
+
+	-- create timer to step progress bar
+	local reverse = false
+	local timer = createTimer(dialog, false)
+	timer.OnTimer = function()
+		if reverse then
+			progress.setPosition(progress.Position-1)
+		else
+			progress.setPosition(progress.Position+1)
+		end
+
+		if progress.Position == progress.Max then
+			reverse = true
+		elseif progress.Position == progress.Min then
+			reverse = false
 		end
 	end
 
-	if proc ~= nil then
-		openProcess(proc.PID)
-		return proc.PID
-	else
-		showMessage('Process not found: ' .. pName)
+	--[[
+	dialog.done = function()
+		timer.setEnabled(false)
+		--dialog.destroy()
+
+		if timer then
+			print('Timer not destroyed')
+			timer.destroy()
+		end
+
+		if timer then
+			print('Timer STILL not destroyed')
+		else
+			print('Timer destroyed')
+		end
 	end
+	]]
+
+	dialog.start = function()
+		timer.setEnabled(true)
+		dialog.ShowModal()
+	end
+
+	dialog.stop = function(finished)
+		timer.setEnabled(false)
+		dialog.ModalResult = 6
+		--[[
+		if not finished then
+			-- '6' is the value returned when mbYes is pressed
+			if messageDialog('Are you sure you want to cancel?', mtInformation, mbYes, mbNo) == 6 then
+				dialog.ModalResult = 6
+				dialog.done()
+			end
+		end
+		]]
+
+		return dialog.ModalResult
+	end
+
+	dialog.pause = function()
+		timer.setEnabled(false)
+	end
+
+	return dialog
+end
+
+-- attaches process
+process.attach = function(pName)
+	local progress = createProgressDialog(pName)
+
+	local PID = nil
+
+	-- FIXME: open in separate thread
+	local thread = createThread(function()
+		local wList = getWindowList()
+
+		local proc = nil
+
+		for _, w in pairs(wList) do
+			if w.Name == pName then
+				proc = w
+				break
+			end
+		end
+
+		progress.pause()
+
+		if proc ~= nil then
+			openProcess(proc.PID)
+			PID = proc.PID
+		else
+			showMessage("Process not found: " .. pName)
+		end
+
+		progress.stop(true)
+	end)
+
+	progress.start()
+	progress.destroy()
+
+	return PID
 end
 
 return process
