9ec282bd32d72b8ebf25c57829fa3eda22ed2970
Jordan Irwin <antumdeluge@gmail.com>
2019-02-25 23:56:47 -0800

    Trainer: Minor change

--- scripts/pages/tools.lua
+++ scripts/pages/tools.lua
@@ -1,7 +1,7 @@
 
-local tools = tabs.addTab()
-tools.setCaption('Tools/Weapons')
-tools.AutoScroll = true
+local pgTools = tabs.addTab()
+pgTools.setCaption('Tools/Weapons')
+pgTools.AutoScroll = true
 
 --- Parent node/header record.
 local parentID = 215
@@ -9,8 +9,8 @@ local recTools = record.get(parentID, true)
 
 local idx = 0
 while idx < recTools.Count do
-	createControl('checkvalue', recTools[idx], tools)
+	createControl('checkvalue', recTools[idx], pgTools)
 	idx = idx + 1
 end
 
-return tools
+return pgTools
