72b0a4e21a0d34942cbc9ff87e1422932581c239
Jordan Irwin <antumdeluge@gmail.com>
2019-02-27 13:40:56 -0800

    Trainer: Remove duplicate call to show errors after window construction

--- scripts/launch.lua
+++ scripts/launch.lua
@@ -19,10 +19,3 @@ local pgTools = dofile('scripts/pages/tools.lua')
 mmu.refreshControls()
 
 mmu.Frame.show()
-
--- Show any errors/warnings from startup
-if #mmu.errors > 0 then
-	for idx, msg in pairs(mmu.errors) do
-		showMessage(msg)
-	end
-end
