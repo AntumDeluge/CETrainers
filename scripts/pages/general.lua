b41e03f295faef4692cc174088f8aeecadf36761
Jordan Irwin <antumdeluge@gmail.com>
2019-02-27 22:41:44 -0800

    Trainer: Set 'Release' flag for general controls

--- scripts/pages/general.lua
+++ scripts/pages/general.lua
@@ -2,8 +2,8 @@
 local pgGeneral = nil
 
 local checkboxes = {
-	{'Instant Death', 'Ends turn as soon as Mega Man is damaged.',},
-	{'Auto End Stage', 'Loads continue screen after turn ends regardless of how many lives are left.',},
+	{'Instant Death', 'Ends turn as soon as Mega Man is damaged.', true},
+	{'Auto End Stage', 'Loads continue screen after turn ends regardless of how many lives are left.', true,},
 }
 
 if mmu.Small then
@@ -13,28 +13,32 @@ else
 	pgGeneral.setCaption('General')
 
 	mmu.tableConcat(checkboxes, {
-		{'Infinite Lives', 'Mega Man\'s lives will not decrease.',},
-		{'Infinite Weapon Energy', 'Special tools & weapons energy will not be depleted when used.',},
-		{'Invincibility', 'Mega Man takes no damage when colliding with enemies.',},
-		{'10+ Lives', 'Mega Man is not limited to a maximum of 9 lives (does not work in shop menu).',},
-		{'No Shot Limit', 'Mega Man can fire more than 3 shots at a time (applies to special weapons).',},
+		{'Infinite Lives', 'Mega Man\'s lives will not decrease.', true,},
+		{'Infinite Weapon Energy', 'Special tools & weapons energy will not be depleted when used.', true,},
+		{'Invincibility', 'Mega Man takes no damage when colliding with enemies.', true,},
+		{'10+ Lives', 'Mega Man is not limited to a maximum of 9 lives (does not work in shop menu).', true,},
+		{'No Shot Limit', 'Mega Man can fire more than 3 shots at a time (applies to special weapons).', true,},
 	})
 end
 
 for _, c in pairs(checkboxes) do
 	local name = nil
 	local helpstring = nil
+	local release = nil
 
 	if type(c) == 'table' and #c > 0 then
 		name = c[1]
 		if #c > 1 then
 			helpstring = c[2]
+			if #c > 2 then
+				release = c[3]
+			end
 		end
 	else
 		name = c
 	end
 
-	local ctrl = mmu.createControl('check', name, pgGeneral)
+	local ctrl = mmu.createControl('check', name, pgGeneral, nil, nil, release)
 
 	if helpstring ~= nil then
 		ctrl.setHelpString('General Controls', helpstring)
